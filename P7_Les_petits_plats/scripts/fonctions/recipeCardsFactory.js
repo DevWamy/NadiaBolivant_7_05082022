8908464a64eddcaf80c6e5936d6fde2fc05dbb55
John Doe <johndoe@example.com>
2022-08-05 20:01:49 +0200

    Init P7 project

---
+++ P7_Les_petits_plats/scripts/fonctions/recipeCardsFactory.js
@@ -0,0 +1,66 @@
+// Modern
+function recipeCardsFactory(recette) {
+    function getRecipeCardDOM() {
+        // Je prends la div qui va contenir les vignettes.
+        const recipeList = document.querySelector('#wrapper-recettes');
+
+        // Je crée le bloc article des recettes.
+        const articleRecipie = `
+            <article class="col-xl-4 col-md-6 article-recette" id="cardRecipe">
+
+                <div class="card">
+
+                    <div class="background"></div>
+
+                        <div class="row descriptif p-3 ">
+
+                            <h5 class="col-md-7 titre mb-3 d-flex align-items-center">${recette.name}</h5>
+
+                            <div class="col-md-5 time mb-3 d-flex align-items-center justify-content-end">
+                                <i class="bi bi-clock"></i> &nbsp;
+                                ${recette.time} min
+                            </div>
+
+                            <ul class="col-md-6 liste mb-3 " id="ingredients-${recette.id}">
+
+                            </ul>
+
+                            <p class="col-md-6 texte">${recette.description.substring(0, 160) + '...'}</p>
+
+                            <p class="col-md-12 texte d-none">Appareil recommandé ${recette.appliance}</p>
+                            <p class="col-md-12 texte d-none">Ustensile recommandé ${recette.ustensils}</p>
+
+                        </div>
+                    </div>
+
+            </article>`;
+
+        // j'insere les blocs tant qu'il y en à l'un après l'autre
+        recipeList.insertAdjacentHTML('beforeEnd', articleRecipie);
+    }
+
+    getRecipeCardDOM();
+
+    // Je boucle sur le tableau d'ingrédients pour les afficher 1 par 1
+    recette.ingredients.forEach((ingredient) => {
+        let listeIngrédients = document.querySelector('#ingredients-' + recette.id);
+
+        let varIngredient = ingredient.ingredient;
+        let varQuantity = ingredient.quantity;
+        let varUnit = ingredient.unit;
+
+        if (varQuantity == undefined) {
+            varQuantity = '';
+        }
+
+        if (varUnit === undefined) {
+            varUnit = '';
+        }
+
+        const listeItem = `
+                <li><strong>${varIngredient} :</strong> ${varQuantity} ${varUnit}</li>
+            `;
+
+        listeIngrédients.insertAdjacentHTML('beforeEnd', listeItem);
+    });
+}
